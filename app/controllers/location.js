ac2098e8e3ca0d6822e5336f1e42ed0ba612bf7c
Jordan Kirkman <jokirkman@vivint.com>
2015-02-02 21:01:21 -0700

    added loading templates, and a loading spinner in the header

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -3,7 +3,14 @@ import Ember from "ember";
 var LocationController = Ember.Controller.extend({
 	locationSearch: null,
 
+	// this toggles a little loading spinner in the header
+	// seemed nice to have when loading non-model data
+	loading: false,
+
 	actions: {
+
+		// this just forwards to the same route with a different url param
+		// ember routing gets our model for us
 		changeLocation: function(){
 			var search = encodeURIComponent(this.get('locationSearch')).toLowerCase();
 			this.transitionToRoute('location', search);
